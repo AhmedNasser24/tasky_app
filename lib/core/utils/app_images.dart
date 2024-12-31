4c4ff0a08cf51dc470b4b083d2727d0f23f998bf
AhmedNasser24 <https://github.com/AhmedNasser24>
2024-12-31 21:03:15 +0200

    change app icon & name

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesLogoPng
+  /// assets/images/logo_png.png
+  static const String imagesLogoPng = "assets/images/logo_png.png";
+
+  /// Assets for imagesLogoSvg
+  /// assets/images/logo_svg.svg
+  static const String imagesLogoSvg = "assets/images/logo_svg.svg";
+}
+
