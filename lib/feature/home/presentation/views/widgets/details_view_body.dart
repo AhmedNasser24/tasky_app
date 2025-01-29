c49923d4beb960c8e346b409fc172c12b103fc33
AhmedNasser24 <https://github.com/AhmedNasser24>
2025-01-29 14:38:07 +0200

    finish title & desc for details

--- lib/feature/home/presentation/views/widgets/details_view_body.dart
+++ lib/feature/home/presentation/views/widgets/details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tasky_app/feature/home/presentation/views/widgets/task_title_and_desc.dart';
 import '../../../../../core/models/task_model.dart';
 import 'details_app_bar.dart';
 import 'details_task_image.dart';
@@ -14,6 +15,10 @@ class DetailsViewBody extends StatelessWidget {
         children: [
           DetailsAppBar(taskModel: taskModel),
           DetailsTaskImage(imgUrl: taskModel.image!),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: TaskTitleAndDesc(taskModel),
+          ),
           
         ],
       ),
